c1cd48084b9a3f1d93152cb560702387b019aacf
hoangpnm <hoangpnm@i-stech.net>
2025-02-10 10:56:21 +0700

    fetch data server component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
 import MainSlider from "./main/main.slider";
 import { Container } from "@mui/material";
 
-export default function HomePage() {
+export default async function HomePage() {
+
+  const res = await fetch("http://localhost:8000/api/v1/tracks/top", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      category: "CHILL",
+      limit: 10
+    })
+  })
+
+  console.log("Check res", await res.json())
   return (
     <Container>
       <MainSlider />
