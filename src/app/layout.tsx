0bd0fb52264365af1c7be643b52a5c9cc64ae58b
hoangpnm <hoangpnm@i-stech.net>
2025-02-14 11:15:32 +0700

    create new track

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,17 @@
 import ThemeRegistry from '@/components/theme-registry/theme.registry';
 import NextAuthWrapper from '@/lib/next.auth.wrapper';
+import { ToastProvider } from '@/utils/toast';
+
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
             <body>
                 <ThemeRegistry>
                     <NextAuthWrapper>
-                        {children}
+                        <ToastProvider>
+                            {children}
+                        </ToastProvider>
                     </NextAuthWrapper>
                 </ThemeRegistry>
             </body>
